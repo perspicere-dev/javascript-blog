445d03061bf410a9f407990e4a2e4d0fe9d44c66
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-04-11 13:38:11 +0200

    add 'add active class' for clickedElement

--- js/script.js
+++ js/script.js
@@ -2,20 +2,25 @@
 //   const links = document.querySelectorAll('.titles a');
 //   console.log('links:', links);
 // });
-const titleClickHandler = function() {
+const titleClickHandler = function(event) {
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  clickedElement.classList.add('active');
+  console.log('clickedElement (with plus): ' + clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for (let activeArticle of activeArticles) {
