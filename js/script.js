587ea2cf915fc5976e71b69675c636d0dfcab601
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-04-11 14:54:07 +0200

    add getAttribiute herf from clicked link

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 //   console.log('links:', links);
 // });
 const titleClickHandler = function(event) {
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 
@@ -14,7 +15,7 @@ const titleClickHandler = function(event) {
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   clickedElement.classList.add('active');
   console.log('clickedElement (with plus): ' + clickedElement);
@@ -27,7 +28,10 @@ const titleClickHandler = function(event) {
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /*[DONE] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
@@ -37,5 +41,5 @@ const titleClickHandler = function(event) {
 const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
-  link.addEventListener('click', titleClickHandler);
+  link.addEventListener('click', titleClickHandler); // to jest handler? Jak zauważy kliknięcie - 'click' - to wykona funkcję titleClickHandler?
 }
